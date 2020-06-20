63eb7ae3276e316e6b9c70693c32ede0efc0ea19
JiaYi Zhang <bbfag3@gmail.com>
2020-06-20 16:58:14 -0700

    gRPC working

--- chatServer.js
+++ chatServer.js
@@ -1,8 +1,25 @@
+const PROTO_PATH = './protos/message_chat.proto'
+
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+);
+var ChatService = grpc.loadPackageDefinition(packageDefinition).ChatService;
+var client = new ChatService.ChatService('localhost:50051', grpc.credentials.createInsecure())
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var chatService = require('../chat-data-service/chatDataServer')
+//var chatService = require('../chat-data-service/chatDataServer')
 
 var serverId = 'msgServer1'
 let local_session = new Map()
@@ -26,7 +43,14 @@ io.on('connection', (socket) => {
         data['message']['timestamp'] = _ts
         data['message']['from'] = getSocketUid(socket.id)
         console.log(data)
-        chatService.addChatTo(data['cId'], data['message'])
+
+        client.NewMessage({
+            cId: data['cId'],
+            msg: data['message']
+        }, function (err, response) {
+            console.log(response)
+        })
+
         io.to(data['cId']).emit('chatMessage',data['message'])
     });
 
@@ -37,18 +61,29 @@ io.on('connection', (socket) => {
         local_session[socket.id] = {'uid': uid}
     })
 
-    //{'cIds': [conversationId], 'timestamps': [timestampOfLastSeen]}
+    //
     socket.on('fetchChat', (data) => {
         console.log(data)
-        let cIds = data['cIds']
-        let timestamps = data['timestamps']
-        let chatToPush = {}
-        for(let c=0;c<cIds.length;c++) {
-            console.log("Joining %s", cIds[c])
-            socket.join(cIds[c])
-            chatToPush[cIds[c]] = chatService.getChatAfter(cIds[c], timestamps[c])
+        let call = client.FetchOfflineChat()
+
+        // let cIds = data['cIds']
+        // let timestamps = data['timestamps']
+        call.on('data', function (d) {
+            console.log(d)
+            socket.emit('fetchChat', d)
+        })
+        // call.on('end', function (d, e) {
+        //     console.log('END')
+        // })
+        //let chatToPush = {}
+        for (let i = 0; i < data.length;i++) {
+            
+            console.log("Joining %s", data[i])
+            socket.join(data[i].cId)
+            call.write(data[i])
+            //chatToPush[cIds[c]] = chatService.getChatAfter(cIds[c], timestamps[c])
         }
-        socket.emit('fetchChat', chatToPush)
+        //socket.emit('fetchChat', chatToPush)
     })
 });
 
